8a2ee8134c470b4fec05378fb86f9f63cdd1a6bf
Sarah Zurmühle <s.zurmuehle@hispeed.ch>
2019-11-14 09:08:04 +0100

    add test classes

---
+++ DonutChart2.js
@@ -0,0 +1,88 @@
+// code taken from: https://apexcharts.com/react-chart-demos/pie-charts/donut-with-pattern/
+// last visited: 9.11.19
+
+/**
+ * Some other chart
+ *
+ * @param something
+ * @visComp
+ */
+class DonutChart2 extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            options: {
+                chart: {
+                    dropShadow: {
+                        enabled: true,
+                        color: '#111',
+                        top: -1,
+                        left: 3,
+                        blur: 3,
+                        opacity: 0.2
+                    }
+                },
+                stroke: {
+                    width: 0,
+                },
+                labels: ["Comedy", "Action", "SciFi", "Drama", "Horror"],
+                dataLabels: {
+                    dropShadow: {
+                        blur: 3,
+                        opacity: 0.8
+                    }
+                },
+                fill: {
+                    type: 'pattern',
+                    opacity: 1,
+                    pattern: {
+                        enabled: true,
+                        style: ['verticalLines', 'squares', 'horizontalLines', 'circles', 'slantedLines'],
+                    },
+                },
+                states: {
+                    hover: {
+                        enabled: false
+                    }
+                },
+                theme: {
+                    palette: 'palette2'
+                },
+                title: {
+                    text: "Favourite Movie Type"
+                },
+                responsive: [{
+                    breakpoint: 480,
+                    options: {
+                        chart: {
+                            width: 200
+                        },
+                        legend: {
+                            position: 'bottom'
+                        }
+                    }
+                }]
+            },
+            series: [44, 55, 41, 17, 15]
+        }
+    }
+
+    render() {
+        return (
+
+
+
+            <div id="chart">
+            <ReactApexChart options={this.state.options} series={this.state.series} type="donut" width="380" />
+          </div>
+
+
+
+        );
+    }
+}
+
+const domContainer = document.querySelector('#app');
+ReactDOM.render(React.createElement(DonutChart), domContainer);
---
+++ pieChart.js
@@ -0,0 +1,47 @@
+// class taken from https://apexcharts.com/react-chart-demos/pie-charts/simple-pie-chart/
+// last visited: 9.11.19
+
+
+/**
+ * Pie chart
+ *
+ * @visComp
+ * @props {integer} breakpoint
+ * @props {integer} width
+ * @props {string}  position	aum.mfa.co2
+ */
+class PieChart extends React.Component {
+      
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      options: {
+        labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
+        responsive: [{
+          breakpoint: this.props.breakpoint, //480,
+          options: {
+            chart: {
+              width: this.props.width //200
+            },
+            legend: {
+              position: this.props.position //'bottom'
+            }
+          }
+        }]
+      },
+      series: [44, 55, 13, 43, 22],
+    }
+  }
+
+  render() {
+    return (
+      <div id="chart">
+        <ReactApexChart options={this.state.options} series={this.state.series} type="pie" width="380" />
+      </div>
+    );
+  }
+}
+
+const domContainer = document.querySelector('#app');
+ReactDOM.render(React.createElement(PieChart), domContainer);
