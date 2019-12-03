0f57e4dffda7ea8403c1d5ac4a8047810cf0491c
Sarah Zurmühle <s.zurmuehle@hispeed.ch>
2019-12-03 21:56:07 +0100

    add new docstring syntax

--- pieChart.js
+++ pieChart.js
@@ -3,16 +3,29 @@ import ReactApexChart from "react-apexcharts";
 import ReactDOM from "react-dom";
 
 // class taken from https://apexcharts.com/react-chart-demos/pie-charts/simple-pie-chart/
-// last visited: 9.11.19
+// last visited: 09.11.19
 
 /**
  * Pie chart
  *
  * @visComp
  * @props {integer} breakpoint [480]
- * @props {integer} width [200]
- * @props {string} position [bottom]
- * @props {dynamic} labels [aum.mfa.co2]
+ * @props {integer} chartWidth [200]
+ * @props {string} legedPosition [bottom]
+ *
+ * @props {dynamic} nameA [aum.mfa.out.PublicVehicles => name]
+ * @props {dynamic} nameB [aum.mfa.out.ResidentialBuildings => name]
+ * @props {dynamic} nameC [aum.mfa.out.OtherBuildings => name]
+ * @props {dynamic} nameD [aum.mfa.out.Infrastructure => name]
+ * @props {dynamic} nameE [aum.mfa.out.Industry => name]
+ *
+ * @props {dynamic} valueA [aum.mfa.out.PublicVehicles => value => energy => TJ]
+ * @props {dynamic} valueB [aum.mfa.out.ResidentialBuildings => value => energy => TJ]
+ * @props {dynamic} valueC [aum.mfa.out.OtherBuildings => value => energy => TJ]
+ * @props {dynamic} valueD [aum.mfa.out.Infrastructure => value => energy => TJ]
+ * @props {dynamic} valueE [aum.mfa.out.Industry => value => energy => TJ]
+ *
+ * @props {input} click [test] (aum.mfa.in.PublicVehicles => value => VehiclesPerCapita => PrivateVehicles/capita)
  */
 class PieChart extends React.Component {
       
@@ -21,7 +34,7 @@ class PieChart extends React.Component {
 
     this.state = {
       options: {
-        labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
+        labels: [this.props.nameA, this.props.nameB, this.props.nameC, this.props.nameD. this.props.nameE, this.props.nameF], //['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
         responsive: [{
           breakpoint: this.props.breakpoint, //480,
           options: {
@@ -34,14 +47,15 @@ class PieChart extends React.Component {
           }
         }]
       },
-      series: [44, 55, 13, 43, 22],
+      series: [this.props.valueA, this.props.valueB, this.props.valueC, this.props.valueD, this.props.valueE], //[44, 55, 13, 43, 22],
     }
   }
 
   render() {
     return (
       <div id="chart">
-        <ReactApexChart options={this.state.options} series={this.state.series} type="pie" width="380" />
+        <ReactApexChart options={this.state.options} series={this.state.series} type="pie" width="auto" {/*width="380"*/} /> 
+        <button onClick={() => alert(this.props.click)}>test<button>
       </div>
     );
   }
