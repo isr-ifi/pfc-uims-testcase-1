141302e7b0228cf09e95b75e91a9480e8039534c
Sarah Zurmühle <s.zurmuehle@hispeed.ch>
2019-12-05 12:33:52 +0100

    define new docstring syntax and add example data jsons to the project

--- pieChart.js
+++ pieChart.js
@@ -11,21 +11,21 @@ import ReactDOM from "react-dom";
  * @visComp
  * @props {integer} breakpoint [480]
  * @props {integer} chartWidth [200]
- * @props {string} legedPosition [bottom]
+ * @props {string} legendPosition [bottom]
  *
- * @props {dynamic} nameA [aum.mfa.out.PublicVehicles => name]
- * @props {dynamic} nameB [aum.mfa.out.ResidentialBuildings => name]
- * @props {dynamic} nameC [aum.mfa.out.OtherBuildings => name]
- * @props {dynamic} nameD [aum.mfa.out.Infrastructure => name]
- * @props {dynamic} nameE [aum.mfa.out.Industry => name]
+ * @props {dynamic} nameA [Team A] (aum.mfa.out.PublicVehicles.name) //TODO:make dots everywhere
+ * @props {dynamic} nameB [Team B] (aum.mfa.out.PrivateVehicles.name)
+ * @props {dynamic} nameC [Team C] (aum.mfa.out.OtherBuildings.name)
+ * @props {dynamic} nameD [Team D] (aum.mfa.out.ResidentialBuildings.name)
+ * @props {dynamic} nameE [Team E] (aum.mfa.out.Industry.name)
  *
- * @props {dynamic} valueA [aum.mfa.out.PublicVehicles => value => energy => TJ]
- * @props {dynamic} valueB [aum.mfa.out.ResidentialBuildings => value => energy => TJ]
- * @props {dynamic} valueC [aum.mfa.out.OtherBuildings => value => energy => TJ]
- * @props {dynamic} valueD [aum.mfa.out.Infrastructure => value => energy => TJ]
- * @props {dynamic} valueE [aum.mfa.out.Industry => value => energy => TJ]
+ * @props {dynamic} valueA [44] (aum.mfa.out.PublicVehicles.value.4.value)
+ * @props {dynamic} valueB [55] (aum.mfa.out.PrivateVehicles.value.4.value)
+ * @props {dynamic} valueC [55] (aum.mfa.out.OtherBuildings.value.4.value)
+ * @props {dynamic} valueD [43] (aum.mfa.out.ResidentialBuildings.value.4.value)
+ * @props {dynamic} valueE [22] (aum.mfa.out.Industry.value.6.value)
  *
- * @props {input} click [1000] (aum.mfa.in.PublicVehicles => value => VehiclesPerCapita => PrivateVehicles/capita)
+ * @props {dynamic} click [1000] (aum.mfa.in.PublicVehicles.value.1.value)
  */
 class PieChart extends React.Component {
       
@@ -34,7 +34,7 @@ class PieChart extends React.Component {
 
     this.state = {
       options: {
-        labels: [this.props.nameA, this.props.nameB, this.props.nameC, this.props.nameD. this.props.nameE, this.props.nameF], //['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
+        labels: [this.props.nameA, this.props.nameB, this.props.nameC, this.props.nameD, this.props.nameE], //['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
         responsive: [{
           breakpoint: this.props.breakpoint, //480,
           options: {
@@ -47,20 +47,24 @@ class PieChart extends React.Component {
           }
         }]
       },
-      series: [this.props.valueA, this.props.valueB, this.props.valueC, this.props.valueD, this.props.valueE], //[44, 55, 13, 43, 22],
-    }
+      series: [this.props.valueA, this.props.valueB, this.props.valueC, this.props.valueD, this.props.valueE], //[44, 55, 55, 43, 22],
+    };
+    this.showAlert = this.showAlert.bind(this);
+  }
+
+  showAlert() {
+    alert(this.props.click)
   }
 
   render() {
     return (
-      <div id="chart">
-        <ReactApexChart options={this.state.options} series={this.state.series} type="pie" width="100%" /> 
-        <button onClick={() => alert(this.props.click)}>test</button>
-      </div>
+        <div id="chart">
+          <ReactApexChart options={this.state.options} series={this.state.series} type="pie" width="100%" />
+          <button onClick={this.showAlert}>test</button>
+        </div>
     );
   }
 }
-
 
 export default PieChart;
 
